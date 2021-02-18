c2dc8efff06263fea793b2338fa149fa764294f4
gigazelle <33764851+gigazelle@users.noreply.github.com>
2021-02-18 13:05:57 -0700

    Update name

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
     globalNav,
     pages: [
       {
-        title: 'Customer Journey Analytics',
+        title: 'Customer Journey Analytics API',
         path: '/'
       },
       {
